c9584a9fd214b5367efea1dd6c0b5246d124bcea
Manuel Aguirre <maguirre@optimeconsulting.com>
2025-01-06 11:36:56 -0500

    Avanzando

---
+++ src/Bundle/Server/OptimeSsoServerBundle.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * (c) Optime Consulting
+ */
+
+namespace Optime\Sso\Bundle\Server;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class OptimeSsoServerBundle extends Bundle
+{
+
+}
---
+++ src/Component/Server/TokenInterface.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * (c) Optime Consulting
+ */
+
+namespace Optime\Sso\Component\Server;
+
+interface TokenInterface
+{
+    public function getToken(): string;
+
+    public function isUserDataRead(): bool;
+
+    public function getUserIdentifier(): string|int;
+
+    public function getUserData(): array;
+
+    public function getRefreshToken(): string;
+
+    public function getExpirationAt(): \DateTimeInterface;
+}
