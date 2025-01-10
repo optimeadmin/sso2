3d3265d2dea4b933e62ae08bf04807a29a214253
Manuel Aguirre <maguirre@optimeconsulting.com>
2025-01-10 13:08:17 -0500

    Avanzando con sso

--- src/Bundle/Client/Factory/UserFactory.php
+++ src/Bundle/Client/Factory/UserFactory.php
@@ -4,6 +4,7 @@ namespace Optime\Sso\Bundle\Client\Factory;
 
 use Optime\Sso\Bundle\Client\Security\SsoData;
 use Optime\Sso\Bundle\Client\Security\User\BasicSsoUser;
+use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class UserFactory implements UserFactoryInterface
@@ -11,5 +12,14 @@ class UserFactory implements UserFactoryInterface
     public function create(SsoData $ssoData): UserFactoryResult
     {
         return new UserFactoryResult(new BasicSsoUser($ssoData));
+    }
+
+    public function configureOptions(
+        OptionsResolver $companyOptions,
+        OptionsResolver $userOptions,
+        OptionsResolver $profileOptions,
+        OptionsResolver $extraOptions,
+    ): void {
+        
     }
 }
