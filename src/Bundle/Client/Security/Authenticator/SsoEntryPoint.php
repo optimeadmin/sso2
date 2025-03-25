e8be2d08a11a08003311ef0c446a97b4eb839c53
Manuel Aguirre <maguirre@optimeconsulting.com>
2025-03-25 14:51:30 -0500

    Mejoras en entry point de sso

--- src/Bundle/Client/Security/Authenticator/SsoEntryPoint.php
+++ src/Bundle/Client/Security/Authenticator/SsoEntryPoint.php
@@ -7,6 +7,7 @@ declare(strict_types=1);
 
 namespace Optime\Sso\Bundle\Client\Security\Authenticator;
 
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
@@ -21,6 +22,17 @@ class SsoEntryPoint implements AuthenticationEntryPointInterface
 
     public function start(Request $request, AuthenticationException $authException = null): Response
     {
-        return new Response($this->twig->render('@OptimeSsoClient/unauthorized.html.twig'));
+        if ('json' === $request->getContentTypeFormat()) {
+            $response = new JsonResponse('Unauthorized');
+        } else {
+            $response = new Response($this->twig->render('@OptimeSsoClient/unauthorized.html.twig', [
+                'exception' => $authException,
+            ]));
+        }
+
+        $response->setStatusCode(Response::HTTP_UNAUTHORIZED);
+        $response->headers->set('X-Error-Message', $authException->getMessage());
+
+        return $response;
     }
 }
